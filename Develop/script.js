5fbc9c08fd513e26cdf28296f8e806333c8c448d
Philip Powers <philip_powers607@hotmail.com>
2021-08-14 17:54:02 -0400

    all confirms work with console.log

--- Develop/script.js
+++ Develop/script.js
@@ -1,56 +1,78 @@
-var upperChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let upparray = upperChar.split("");
-var lowerChar = "abcdefghijklmnopqrstuvwxyz";
-let lowarray = lowerChar.split("");
-var numberChar = "1234567890";
-let numarray = numberChar.split("");
-var specChar = "!@#$%^&*_-+~";
-let specarray = specChar.split("");
-
-
-
-var allPasswordCharacters = [];
-
-
-
-// Assignment code here
-function generatePassword() {
-  var charLength = prompt("How many Characters would you like in your password?");
-  if (charLength < 8 || charLength > 128) {
-    alert("Please choose between 8 and 128");
-    generatePassword();
-  }
-  var upperCase = confirm("Would you like uppercase letters?");
-    console.log(upperCase);
-  if (upperCase) {
-    allPasswordCharacters.push(upparray);
-}
-  var lowerCase = confirm("Would you like lowercase letters?");
-    console.log(lowerCase);
-  if (lowerCase) {
-    allPasswordCharacters.push(lowarray);
-}
-  var numbers = confirm("Would you like numbers?");
-  if (numbers) {
-    allPasswordCharacters.push(numarray);
-}
-  var specialLetters = confirm("Would you like to add special Characters?");
-  if (specialLetters) {
-    allPasswordCharacters.push(specarray);
-    console.log(allPasswordCharacters);
-}
-
-  randomized(charLength);
-  return "";
-}
-
-
-
-// Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+  
+    upperChar = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')];
+    lowerChar = ['abcdefghijklmnopqrstuvwxyz'.split('')];
+    numberChar = ['1234567890'.split('')];
+    specChar = ['!@#$%^&*~'.split('')];
 
+  
 
+function generatePassword() {
+  // variables to determine how each array is represented
+
+   var allPasswordCharacters = [];
+   var userPass = [];
+  
+  // one array to hold all characters
+  
+  
+  
+  // determines which and how many characters there are.
+  var length = prompt ("How many Characters would you like in your password?");
+
+  if (length < 8 || length > 128) {
+    alert("Please choose between 8 and 128");
+    generatePassword();
+    };
+    // console.log(length);
+
+  var upperCharact = confirm("Would you like uppercase letters?");
+  var lowerCharact = confirm("Would you like lowercase letters?");
+  var numberCharact = confirm("Would you like numbers?")
+  var specCharact = confirm("Would you like to add special Characters?");
+
+    if (!upperCharact && !lowerCharact && !numberCharact && !specCharact) {
+      alert("Please choose one character type!");
+      return;
+    }
+
+    
+  
+  if (upperCharact) {
+    allPasswordCharacters .push(...upperChar);
+}
+  
+  if (lowerCharact) {
+    allPasswordCharacters .push(...lowerChar);
+}
+  ;
+  if (numberCharact) {
+    allPasswordCharacters .push(...numberChar);
+}
+  
+  if (specCharact) {
+    allPasswordCharacters .push(...specChar);
+}
+
+  // console.log(allPasswordCharacters);
+   
+  
+
+    //  randomized(allPasswordCharacters);
+    return userPass ;
+  }
+
+// Assignment code here
+
+ function randomized(length) {
+for (var i = 0; i < length.length; i++) {
+  userPass.push(allPasswordCharacters[Math.floor(Math.random() * allPasswordCharacters.length)]);
+  var password = userPass.join('');
+  }
+  console.log(userPass);
+}
+// Get references to the #generate element
 
 // Write password to the #password input
 function writePassword() {
@@ -60,6 +82,6 @@ function writePassword() {
   passwordText.value = password;
 
 }
-// generateBtn.addEventListener("click", charLengthPrompt);
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
