df40759c791d4a9a504d28b816a076849e6fb9ae
Philip Powers <philip_powers607@hotmail.com>
2021-08-15 20:16:10 -0400

    fixed for loop to create password

--- Develop/script.js
+++ Develop/script.js
@@ -1,87 +1,98 @@
 var generateBtn = document.querySelector("#generate");
+console.log(generateBtn);
+generateBtn.addEventListener("click", writePassword);
+  // Arrays that can be put into password generator.
+    upperChar =  'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    lowerChar =  'abcdefghijklmnopqrstuvwxyz';
+    numberChar = '0123456789';
+    specChar =   "`~!@#$%^&*_-+";
 
-  
-    upperChar = ['ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('')];
-    lowerChar = ['abcdefghijklmnopqrstuvwxyz'.split('')];
-    numberChar = ['1234567890'.split('')];
-    specChar = ['!@#$%^&*~'.split('')];
+  //declaration of variables.
+    var length = "";
+    var upperCharact;
+    var lowerCharact;
+    var numberCharact;
+    var specCharact;
 
-  
+    
 
 function generatePassword() {
   // variables to determine how each array is represented
-
-   var allPasswordCharacters = [];
-   var userPass = [];
-  
-  // one array to hold all characters
-  
-  
-  
-  // determines which and how many characters there are.
-  var length = prompt ("How many Characters would you like in your password?");
-
-  if (length < 8 || length > 128) {
+  var length = (prompt ("How many Characters would you like in your password?"));
+    if (length < 8 || length > 128) {
     alert("Please choose between 8 and 128");
     generatePassword();
     };
-    // console.log(length);
-
+    // confirms each element to put into password.
   var upperCharact = confirm("Would you like uppercase letters?");
   var lowerCharact = confirm("Would you like lowercase letters?");
-  var numberCharact = confirm("Would you like numbers?")
+  var numberCharact = confirm("Would you like numbers?");
   var specCharact = confirm("Would you like to add special Characters?");
-
-    if (!upperCharact && !lowerCharact && !numberCharact && !specCharact) {
-      alert("Please choose one character type!");
-      return;
+  
+  
+   // one place for all arrays.
+  var allPasswordCharacters = [];
+  
+    //confirm password character choices.
+    if (upperCharact) {
+      allPasswordCharacters += upperChar;
     }
+    
+    if (lowerCharact) {
+      allPasswordCharacters += lowerChar;
+    }
+
+    if (numberCharact) {
+      allPasswordCharacters += numberChar;
+    }
+
+    if (specCharact) {
+      allPasswordCharacters += specChar;
+    }
+      if (!upperCharact && !lowerCharact && !numberCharact && !specCharact) {
+      alert("Please choose one character type!");
+     return;
+    };
+    // console.log(allPasswordCharacters);
+
+    var userPass = "";
+
+    for (var i = 0; i < length; i++) {
+    userPass += allPasswordCharacters.charAt(Math.floor(Math.random() * allPasswordCharacters.length));
+      // console.log(userPass);
+    }
+
+    return userPass;
+  }
+
+  function writePassword() {
+
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+
+    passwordText.value = password;
+
+  }
 
     
   
-  if (upperCharact) {
-    allPasswordCharacters .push(...upperChar);
-}
   
-  if (lowerCharact) {
-    allPasswordCharacters .push(...lowerChar);
-}
-  ;
-  if (numberCharact) {
-    allPasswordCharacters .push(...numberChar);
-}
-  
-  if (specCharact) {
-    allPasswordCharacters .push(...specChar);
-}
+   function randomized(length) {
+    
+    
+   }
 
-  // console.log(allPasswordCharacters);
-   
   
 
-    //  randomized(allPasswordCharacters);
-    return userPass ;
-  }
+  
 
 // Assignment code here
 
- function randomized(length) {
-for (var i = 0; i < length.length; i++) {
-  userPass.push(allPasswordCharacters[Math.floor(Math.random() * allPasswordCharacters.length)]);
-  var password = userPass.join('');
-  }
-  console.log(userPass);
-}
+ 
 // Get references to the #generate element
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
